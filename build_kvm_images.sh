617119509df857ec6a99713eb366cc844cf59205
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2024-01-30 10:19:41 +0800

    Update build_kvm_images.sh

--- build_kvm_images.sh
+++ build_kvm_images.sh
@@ -10,3 +10,14 @@ if [ ${#links[@]} -gt 0 ]; then
 else
     echo "没有找到KVM镜像"
 fi
+sudo apt-get install -y libguestfs-tools rng-tools curl
+sudo apt-get install -y libguestfs-tools rng-tools curl --fix-missing
+curl -o rebuild_qcow2.sh https://raw.githubusercontent.com/spiritLHLS/pve/main/back/rebuild_qcow2.sh
+chmod 777 rebuild_qcow2.sh
+for image in "${links[@]}"; do
+  curl -o $image "https://down.idc.wiki/Image/realServer-Template/current/qcow2/$image"
+  chmod 777 $image
+done
+for image in "${links[@]}"; do
+  ./rebuild_qcow2.sh $image
+done
