8086e6f0052b8fa75043fdd8257ea2e52743fc1c
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2024-01-30 10:33:02 +0800

    Update and rename build_kvm_images.sh to main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 #from https://github.com/oneclickvirt/pve_kvm_images
 
-
+sudo apt-get install -y libguestfs-tools rng-tools curl
+sudo apt-get install -y libguestfs-tools rng-tools curl --fix-missing
+curl -o rebuild_qcow2.sh https://raw.githubusercontent.com/oneclickvirt/pve_kvm_images/main/rebuild_qcow2.sh
+chmod 777 rebuild_qcow2.sh
 links=($(curl -s -m 6 https://down.idc.wiki/Image/realServer-Template/current/qcow2/ | grep -o '<a href="[^"]*">' | awk -F'"' '{print $2}' | sed -n '/qcow2$/s#/Image/realServer-Template/current/qcow2/##p'))
 if [ ${#links[@]} -gt 0 ]; then
     for link in "${links[@]}"; do
@@ -10,10 +13,6 @@ if [ ${#links[@]} -gt 0 ]; then
 else
     echo "没有找到KVM镜像"
 fi
-sudo apt-get install -y libguestfs-tools rng-tools curl
-sudo apt-get install -y libguestfs-tools rng-tools curl --fix-missing
-curl -o rebuild_qcow2.sh https://raw.githubusercontent.com/oneclickvirt/pve_kvm_images/main/rebuild_qcow2.sh
-chmod 777 rebuild_qcow2.sh
 for image in "${links[@]}"; do
   curl -o $image "https://down.idc.wiki/Image/realServer-Template/current/qcow2/$image"
   chmod 777 $image
